b6d087de4d2e0b168becf6fc27fe2e397909a26b
ales124 <89366757+ales124@users.noreply.github.com>
2021-09-23 00:14:19 -0300

    cambio prueba2
    a

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -55,7 +55,7 @@
 
                             <div class="ml-12">
                                 <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
-                                    hola mundo
+                                    hola mundo blas
                                 </div>
                             </div>
                         </div>
