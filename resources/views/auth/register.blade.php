8247d985dd32a583ce52234eec1c140ea7a311f5
Cloudxz1 <89432119+Cloudxz1@users.noreply.github.com>
2021-10-13 20:04:39 -0300

    1er requerimiento iniciar sesion
    - Se añade los roles tanto en la base de datos como en las vistas para Administador/Jefe de Carrera/Alumno
    - Validar rut funcional

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
+                <div class="card-header">{{ __('Registrar Usuario') }}</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('register') }}">
@@ -87,6 +87,14 @@
                             <div class="col-md-6">
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
                             </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="form-control-label" style="color: black">Rol</label>
+                            <select class="form-control" name="rol" id="rol">
+                                <option value="Administrador">Administrador</option>
+                                <option value="Jefe de Carrera">Jefe de carrera</option>
+                                <option value="Alumno">Alumno</option>
+                            </select>
                         </div>
 
                         <div class="form-group row mb-0">
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,21 +3,87 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        @if (Auth::user()->rol == "Administrador")
+        <div class="card-deck">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
+                <i class="fas fa-users fa-10x text-center"></i>
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+                    <h5 class="card-title text-center" style = "color:red">Administrar usuarios</h5>
+                    <small class="text-muted">Permite o crear/editar/deshabilitar usuarios del sistema.</small>
+                </div>
+                <div class="card-footer">
+                    <a href="/usuario" class="btn btn-info btn-block">IR</a>
+                </div>
+            </div>
+            <div class="card">
+                <i class="fas fa-graduation-cap fa-10x text-center"></i>
+                <div class="card-body">
+                    <h5 class="card-title text-center">Administrar Carreras</h5>
+                    <small class="text-muted">Permite crear y/o editar carreras en el sistema.</small>
+                </div>
+                <div class="card-footer">
+                    <a href="" class="btn btn-info btn-block">IR</a>
                 </div>
             </div>
         </div>
+        @elseif (Auth::user()->rol == "Jefe de Carrera")
+        <div class="card-deck">
+            <div class="card">
+                <i class="fas fa-users fa-10x text-center"></i>
+                <div class="card-body">
+                    <h5 class="card-title text-center">Carga masiva de estudiantes</h5>
+                    <small class="text-muted">Permite realizar una carga masiva de estudiantes al sistema.</small>
+                </div>
+                <div class="card-footer">
+                    <a href="" class="btn btn-info btn-block">IR</a>
+                </div>
+            </div>
+            <div class="card">
+                <i class="fas fa-search fa-10x text-center"></i>
+                <div class="card-body">
+                    <h5 class="card-title text-center">Buscar estudiante</h5>
+                    <small class="text-muted">Permite buscar un estudiante mediante su RUT.</small>
+                </div>
+                <div class="card-footer">
+                    <a href="" class="btn btn-info btn-block">IR</a>
+                </div>
+            </div>
+            <div class="card">
+                <i class="fas fa-check-double fa-10x text-center"></i>
+                <div class="card-body">
+                    <h5 class="card-title text-center">Resolver solicitudes</h5>
+                    <small class="text-muted">Permite visualizar todas las solicitudes de caracter "Pendiente".</small>
+                </div>
+                <div class="card-footer">
+                    <a href="" class="btn btn-info btn-block">IR</a>
+                </div>
+            </div>
+
+            <div class="card">
+                <i class="fas fa-info fa-10x text-center"></i>
+                <div class="card-body">
+                    <h5 class="card-title text-center">Estadísticas del sistema</h5>
+                    <small class="text-muted">Permite visualizar mediante gráficos las distintas solicitudes del sistema.</small>
+                </div>
+                <div class="card-footer">
+                    <a href="" class="btn btn-info btn-block">IR</a>
+                </div>
+            </div>
+        </div>
+        @elseif (Auth::user()->rol == "Alumno")
+        <div class="card-deck">
+            <div class="card">
+                <i class="fas fa-tasks fa-10x text-center"></i>
+                <div class="card-body">
+                    <h5 class="card-title text-center">Gestión de solicitudes</h5>
+                    <small class="text-muted">Permite o crear/editar/anular solicitudes especiales.</small>
+                </div>
+                <div class="card-footer">
+                    <a href="" class="btn btn-info btn-block">IR</a>
+                </div>
+            </div>
+        </div>
+        @endif
     </div>
 </div>
 @endsection
