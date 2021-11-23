2e0e9f211a1d4410b33ab06020a34f9a76c936d9
ales124 <89366757+ales124@users.noreply.github.com>
2021-11-23 18:56:04 -0300

    conexion base datos

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,4 +45,8 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function carrera(){
+        return $this->belongsTo(Carrera::class);
+    }
 }
